ee93f7b0da8698910ab72de7dc38fd95a092d709
Willy Leloutre <willyleloutre@MacBook-Pro-de-Willy.local>
2016-06-21 11:51:52 +0200

    test rafik

---
+++ index.js
@@ -0,0 +1,9 @@
+/**********ATTENTE DOCUMENT READY**************/
+$(document).ready(function(){
+/***********************************************/
+	$('.scrollTo').click( function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
